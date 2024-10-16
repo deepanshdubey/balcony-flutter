48f3abb108b686fadcb92061a1cd1dd6274cc748
Amol <amoldesai.ard@gmail.com>
2024-10-16 13:32:09 +0530

    primary button added

---
+++ lib/widget/primary_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final bool isLoading, enabled;
+  final Color? backgroundColor;
+
+  const PrimaryButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    this.isLoading = false,
+    this.enabled = true,
+    this.backgroundColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: isLoading ? null : onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 20.w),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.r),
+        ),
+        backgroundColor: backgroundColor ?? Theme.of(context).primaryColor,
+      ),
+      child: isLoading
+          ? SizedBox(
+              width: 24,
+              height: 24,
+              child: CircularProgressIndicator(
+                color: Theme.of(context)
+                    .primaryColor
+                    .withOpacity(enabled ? 1 : 0.5),
+                strokeWidth: 2.r,
+              ),
+            )
+          : Text(
+              text,
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                  fontSize: 18.spMin),
+            ),
+    );
+  }
+}
